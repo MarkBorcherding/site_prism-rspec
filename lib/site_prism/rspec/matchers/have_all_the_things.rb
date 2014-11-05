f4639726ecb13abc104a1c1d5127439fb25e8ed7
Mark Borcherding <markborcherding@gmail.com>
2014-11-04 19:57:46 -0600

    add negative assertion of have all the things

--- lib/site_prism/rspec/matchers/have_all_the_things.rb
+++ lib/site_prism/rspec/matchers/have_all_the_things.rb
@@ -11,6 +11,10 @@ module SitePrism
           "Unable to find all the things for #{actual.class}.\n  Could not find: #{missing_items.join ', '}"
         end
 
+        def failure_message_when_negated
+          "Didn't expected to find all the things for #{actual.class}."
+        end
+
         protected
 
         def expected_items
--- spec/features/have_all_the_things_spec.rb
+++ spec/features/have_all_the_things_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-feature "Widget management" do
+feature "Having all the things" do
   let(:site) { TestSite.new }
 
   before { site.home.load }
@@ -9,9 +9,19 @@ feature "Widget management" do
     expect(site.home.section_with_all_the_things).to have_all_the_things
   end
 
+  scenario "when we expect to not have all the things" do
+    expect(site.home.section_with_missing_things).to_not have_all_the_things
+  end
+
   scenario "when we do not have all the things" do
     expect {
       expect(site.home.section_with_missing_things).to have_all_the_things
     }.to raise_exception(RSpec::Expectations::ExpectationNotMetError, /Could not find: thing_c/)
+  end
+
+  scenario "when we expect to not find al the things" do
+    expect {
+      expect(site.home.section_with_all_the_things).to_not have_all_the_things
+    }.to raise_exception(RSpec::Expectations::ExpectationNotMetError, /Didn't expected to find all the things for SectionWithThings/)
   end
 end
